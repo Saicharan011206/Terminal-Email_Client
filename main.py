6d386e703265272ea319a53ef3d29a32fa8132da
Saicharan011206 <saicharan011206@gmail.com>
2025-11-22 22:41:01 +0530

    email sending is added and works just fine

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 print("Welcome to your email_client!")
+import smtplib
+import imaplib
 def send_email():
     print("Sending email...")
+    sender_email = input("Enter your email: ")
+    receiver_email = input("Enter recipient email: ")
+    password = input("Enter your password:(app password if your using gmail) ")
+    subject = input("Enter subject: ")
+    body = input("Enter email body: ")
+    message = f"Subject: {subject}\n\n{body}"
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
 def receive_email():
     print("Receiving email...")
 def main_menu():
@@ -19,3 +35,5 @@ def main_menu():
             break
         else:
             print("Invalid choice. Please try again.")
+if __name__ == "__main__":
+    main_menu()
