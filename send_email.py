01b03a23bab10446705f4814309de4c06a010be0
Saicharan011206 <saicharan011206@gmail.com>
2025-11-26 20:56:28 +0530

    Added option to generate server and port address based on user domain,error handling is polished to represent connection,authentication,receiver errors

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import pwinput
 import smtplib
+from login_utils import get_smtp_server
 def send_email(user_email, password):
     print("=== Sending Email ===")
     receiver_email = input("Enter recipient's email address: ").strip()
@@ -37,11 +38,25 @@ def send_email(user_email, password):
 
     message = f"Subject: {subject}\n\n{body}"
 
+    smtp_server,port=get_smtp_server(user_email)
+    if smtp_server is None:
+        print("Unsupported email provider.")
+        return False
     try:
-        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        with smtplib.SMTP(smtp_server,port,timeout=10) as server:
             server.starttls()
             server.login(user_email, password)
             server.sendmail(user_email, receiver_email, message)
         print("Email sent successfully!")
+        return True
+    except smtblib.SMTPAuthenticationError:
+        print("Authentication failed. Please check your email and password.")
+    except smtplib.SMTPConnectError:
+        print("Failed to connect to the SMTP server. Please check your internet connection.")
+    except smtplib.SMTPRecipientsRefused:
+        print("The recipient's email address was refused by the server.")
+    except smtplib.SMTPException as e:
+        print(f"An SMTP error occurred: {e}")
     except Exception as e:
-        print(f"Failed to send email: {e}")
+        print(f"An unexpected error occurred: {e}")
+    return False
